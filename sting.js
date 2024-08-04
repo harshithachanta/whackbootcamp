394bb4b318dc2fc57f79a4dc5467f3256644d23e
[harshithachanta] <[harshithgood3145@gmail.com]>
2024-08-04 14:54:12 +0530

    ok

--- sting.js
+++ sting.js
@@ -1,9 +1,48 @@
-let string="harshith";
-console.log(string.length);
-console.log(string.split(''));
-console.log(string.split('').reverse());
-console.log(string.split('').reverse().join(''));
-console.log(string.charAt(1));
-console.log(string.charCodeAt(2));
-console.log(string.at(2));
-console.log(string.lastIndexOf());
+// let string="harshith";
+// console.log(string.length);
+// console.log(string.split(''));
+// console.log(string.split('').reverse());
+// console.log(string.split('').reverse().join(''));
+// console.log(string.charAt(1));
+// console.log(string.charCodeAt(2));
+// console.log(string.at(2));
+// console.log(string.lastIndexOf());
+let names = "tejeswararaoesw"
+
+var num = 1234
+
+// console.log(names + " " + num)
+
+console.log(names.indexOf("esw"))
+
+// console.log(names.lastIndexOf("esw"))
+
+// console.log(names.slice(5,10))
+
+// console.log(names.replaceAll("esw", "wse"))
+
+// console.log(names[1])
+
+// for(let i=0;i<names.length;i++){
+//     console.log(names[i])
+// }
+
+// console.log(names.split('a'))
+
+// console.log(typeof (num))
+
+// var num=String(num)
+
+// var num = num.toString()
+
+// console.log(num)
+
+// console.log(typeof (num))
+
+// num = Number(num)
+
+// console.log(typeof (num))
+
+// console.log(Math.tan(1))
+
+// console.log(typeof(null))
